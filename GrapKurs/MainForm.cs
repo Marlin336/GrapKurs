cb17f542bb5be1f5bed775915e905401090a2b54
Marlin <nik.poludennyy@mail.ru>
2018-06-05 23:29:04 +0300

    Фиксация

--- GrapKurs/MainForm.cs
+++ GrapKurs/MainForm.cs
@@ -129,23 +129,6 @@ namespace GrapKurs
                 }
             }
         }
-        void Rasterize(Point p0, Point p1, Bitmap bitmap, Color color, int[] ybuffer)
-        {
-            if (p0.x > p1.x)
-            {
-                Swap(ref p0, ref p1);
-            }
-            for (int x = p0.x; x <= p1.x; x++)
-            {
-                float t = (x - p0.x) / (float)(p1.x - p0.x);
-                int y = (int)(p0.y * (1 - t) + p1.y * t);
-                if (ybuffer[x] < y)
-                {
-                    ybuffer[x] = y;
-                    bitmap.SetPixel(x, 0, color);
-                }
-            }
-        }
 
         private void DrawCircleBrez(int x0, int y0, int rad, Bitmap bitmap)//Алгоритм Брезенхэма
         {
